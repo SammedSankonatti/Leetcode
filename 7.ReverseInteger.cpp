70035db0df2819aa428dafb25fd11b6100c2e802
sammed-sankonatti <myofficialtrack@gmail.com>
2020-12-15 23:23:05 +0530

    reverse integer using string

--- 7.ReverseInteger.cpp
+++ 7.ReverseInteger.cpp
@@ -6,20 +6,54 @@ class Solution
 public:
     int reverse(int x)
     {
-        int res=0;
+        long res=0;
         while(x)
         {
             res= res*10+x%10;
             x/=10;
         }
 
+        if(res<=INT_MIN || res>=INT_MAX)
+        {
+            res=0;
+        }
+
         return res;
     }
+
+    int reverseWithString(int x);
 };
+
+int Solution::reverseWithString(int x)
+{
+    int res=x;
+    if(x<INT_MIN || x>INT_MAX)
+    {
+        return 0;
+    }
+        
+    
+    if(x<0)
+        res=-res;
+    
+    string str =to_string(res);
+    
+    ::reverse(str.begin(),str.end());
+
+    if(x<0)
+    {
+        str.insert(str.begin(), '-');
+    }
+    x= stoi(str);
+
+    return x;
+}
 
 int main()
 {
     Solution rev;
     cout<<rev.reverse(-123);
+    cout<<"using string"<<endl;
+    cout<<rev.reverseWithString(-456);
 
 }
