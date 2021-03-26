3546e92ebaa4f779f3cd1dbbe7fcf1264e1ad8b1
sammed-sankonatti <myofficialtrack@gmail.com>
2021-03-27 03:32:51 +0530

    max depth

---
+++ C++/104.maxDepth.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0; // edge case
+
+        int ans = 0;
+        queue<TreeNode *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            ++ans;
+            queue<TreeNode *> newq;
+            for (int i = 0, n = q.size(); i < n; ++i)
+            {
+                TreeNode *node = q.front();
+                q.pop();
+                if (node->left != NULL)
+                    q.push(node->left);
+                if (node->right != NULL)
+                    q.push(node->right);
+            }
+        }
+        return ans;
+    }
+};
